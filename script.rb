fe0c59c2bc7084deb208ac728ba062a7ee54cb44
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-05-16 18:22:47 -0400

    player can play a complete round as the CodeBreaker

--- script.rb
+++ script.rb
@@ -42,28 +42,43 @@ class CodeMaker
     
 
     def initialize
-        initial_display
         @code = get_code
+        p @code
         @guesses_remaining = 12
+        initial_display
     end
 
     def initial_display
         puts "\n\n\n\n\n\n\n\n\n\nYou are playing this round as the Code-Breaker.  You must attempt to guess the computer's code."
-        sleep 3
+        sleep 2
         puts "\nThe following colors will be represented with the corresponding code:"
-        sleep 3
+        sleep 2
         puts "\nRed: 1, Blue: 2, Green: 3, Yellow: 4, Orange: 5, Purple: 6"
-        sleep 3
-        puts "\nThe computer's code will be a 4-digit number, with each digit being a number between 1-6. Example: 6142"
+        sleep 2
+        puts "\nThe computer's code will be displayed as a 4-digit number, with each digit being a number between 1-6. Example: 6142"
+        begin_attempts
     end
 
     def get_code
-        [rand(1..6), rand(1..6), rand(1..6), rand(1..6)]
+        code = [rand(1..6), rand(1..6), rand(1..6), rand(1..6)]
+        code.uniq.length == 4 ? code : get_code
+    end
+
+    def begin_attempts
+        while @guesses_remaining > 0  
+            puts "\nEnter your guess:"
+            guess = get_guess
+            if guess == @code.join
+                return round_won
+            end
+            display_progress(guess)
+        end
+        round_lost
     end
 
     def get_guess
-        guess = gets.chomp.to_i
-        if guess.to_s.gsub(/[1-6]{4}/, '').empty? 
+        guess = gets.chomp
+        if guess.gsub(/[1-6]{4}/, '').empty? 
             guess
         else
             invalid_guess
@@ -71,8 +86,23 @@ class CodeMaker
         end
     end
 
+    def display_progress(guess)
+        correct_position = guess.split('').select.with_index { |num, ind| num.to_i == @code[ind] }.length
+        correct_color = guess.split('').uniq.count { |num| @code.include?(num.to_i)} - correct_position
+        @guesses_remaining -= 1
+        puts "\nYour guess: #{guess}     Colors in correct positions: #{correct_position}     Correct colors in incorrect positions: #{correct_color}     Guesses remaining: #{@guesses_remaining}"
+    end
+
+    def round_won
+        puts "\n\n\n\n\n\n\n\n\n\nYou guessed the code!  You won the round!"
+    end
+
+    def round_lost
+        puts "\n\n\n\n\n\n\n\n\n\nYou've run out of guesses.  You lost the round!\n\n"
+    end
+
     def invalid_guess
-        puts "\nInvalid input.  Please enter a 4 digit number, with each digit being a number between 1-6. Example: 6142"
+        puts "\nInvalid input.  Enter a 4 digit number, with each digit being a number between 1-6. Example: 6142"
     end
 end
 
